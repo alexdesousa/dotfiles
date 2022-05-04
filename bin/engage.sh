db46987ebbd98d3c6266a64f3f2d9617a325194d
Alex de Sousa <alex@thebroken.link>
2022-05-04 12:03:47 +0200

    :sparkles: Added extra configuration for git

--- bin/engage.sh
+++ bin/engage.sh
@@ -4,4 +4,7 @@ set -e
 
 PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
-ansible-playbook -i "${PROJECT_ROOT}/hosts" "${PROJECT_ROOT}/engage.yml" --ask-become-pass --ask-vault-pass
+ansible-playbook \
+  -i "${PROJECT_ROOT}/hosts" "${PROJECT_ROOT}/engage.yml" \
+  --ask-become-pass --ask-vault-pass \
+  -e "ansible_python_interpreter=$(which python3)"
